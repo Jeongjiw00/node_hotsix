d1b3ed2133a4f4ce0099c6410b33c4ef2093ffcd
wjdrl3122 <wjdrl3122@gmail.com>
2023-01-04 23:09:47 +0900

    새암님 빼고 연결해버리기~!

--- app.js
+++ app.js
@@ -14,7 +14,17 @@ app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.use(express.static(__dirname + "/views")); //정적파일, 이미지파일
 
-//경로
+
+// 유저 마이 페이지
+app.get("/user", (req, res) => {
+  res.render("userMyPage.ejs");
+});
+
+app.get("/owner", (req, res) => {
+  res.render("ownerMyPage.ejs");
+});
+
+// 유저가 빨래 신청 ejs 연결
 app.get("/laundry/apply", (req, res) => {
   res.render("index_jw.ejs", { test: false });
 });
@@ -22,15 +32,24 @@ app.get("/laundry/:id", (req, res) => {
   res.render("index_jw.ejs", { test: true });
 });
 
-app.get("/owner", (req, res) => {
-  res.render("ownerPage.ejs", { temp: 0 });
-})
 app.get("/owner/laundries", (req, res) => {
   res.render("ownerPage.ejs", { temp: 1 });
 })
 app.get("/owner/laundry", (req, res) => {
   res.render("ownerPage.ejs", { temp: 2 });
 })
+
+
+// 로그인 페이지 ejs 연결
+app.get("/logIn", (req, res) => {
+  res.render("logIn.ejs");
+});
+
+// 회원가입 페이지 ejs 연결
+app.get("/signUp", (req, res) => {
+  res.render("signUp.ejs");
+});
+
 
 //메인페이지 나중에 변경해야함~
 // app.get("/", (req, res) => {
